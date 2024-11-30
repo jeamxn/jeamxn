50989ec84f2bf7a7568fbe95252b2ed3d7e553da
jeamxn <admin@chicken-moo.com>
2024-11-30 18:14:52 +0900

    feat: allow figma

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,22 +9,22 @@ const config: Config = {
     "./src/provider/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      "inherit": "inherit",
-      "transparent": "transparent",
-      "background": "rgb(var(--color-background) / <alpha-value>)",
-      "primary": "rgb(var(--color-primary) / <alpha-value>)",
-      "text": "rgb(var(--color-text) / <alpha-value>)",
-      "white": "rgb(var(--color-white) / <alpha-value>)",
-      "green": "rgb(var(--color-green) / <alpha-value>)",
-      "white-light": "rgb(var(--color-white-light) / <alpha-value>)",
-      "text-light": "rgb(var(--color-text-light) / <alpha-value>)",
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "inherit": "inherit",
+        "transparent": "transparent",
+        "background": "rgb(var(--color-background) / <alpha-value>)",
+        "primary": "rgb(var(--color-primary) / <alpha-value>)",
+        "text": "rgb(var(--color-text) / <alpha-value>)",
+        "white": "rgb(var(--color-white) / <alpha-value>)",
+        "green": "rgb(var(--color-green) / <alpha-value>)",
+        "white-light": "rgb(var(--color-white-light) / <alpha-value>)",
+        "text-light": "rgb(var(--color-text-light) / <alpha-value>)",
       },
     },
   },
